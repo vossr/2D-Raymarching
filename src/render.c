8646cbf30442a9626ffde58ce73551652b41e4ad
rpehkone <pehkonenroni@gmail.com>
2020-08-16 19:48:21 +0300

    Implemented base of texture loading and made texture access fully work with rendering and walls

--- src/render.c
+++ src/render.c
@@ -1,5 +1,47 @@
 #include "wolf.h"
 
+int		***load_texture(void)
+{
+	int ***texture;
+	int	i;
+	int	x;
+	int	y;
+
+	texture = (int***)malloc(sizeof(int**) * 4);
+	i = 0;
+	while (i < 4)
+	{
+		texture[i] = (int**)malloc(sizeof(int*) * 64);
+		y = 0;
+		while (y < 64)
+		{
+			texture[i][y] = (int*)malloc(sizeof(int) * 64);
+			x = 0;
+			while (x < 64)
+			{
+				if (i == 0)
+					texture[i][y][x] = 0xFFFFFF;
+				else if (i == 1)
+					texture[i][y][x] = 0xFF0000;
+				else if (i == 2)
+					texture[i][y][x] = 0xFF00;
+				else if (y < 32 && x < 32)
+					texture[i][y][x] = 0xFF0000;
+				else if (y < 32)
+					texture[i][y][x] = 0xFF00;
+				else if (x < 32)
+					texture[i][y][x] = 0xFF;
+				else
+					texture[i][y][x] = 0xFFFFFF;
+				x++;
+			}
+			y++;
+		}
+		i++;
+	}
+	return (texture);
+}
+
 void	megapixel_put(int x, int y, int color)
 {
 	int	i;
@@ -44,20 +86,26 @@ void	map_print(t_float_xy location, t_int_xy map_size, int **map)
 
 void	put_texture(int line_x, t_float_xy line, int texture_id, float texture_x)
 {
-	int		y;
+	static int	***texture = NULL;
+	int			y;
 
+	if (!texture)
+		texture = load_texture();
+	y = 0;
+	while (y < line.x)
+	{
+		pixel_put(line_x, y, 0x7777FF);
+		y++;
+	}
 	y = line.x;
 	while (y < line.y)
 	{
-		if (texture_id == 0)
-			pixel_put(line_x, y, texture_x * 0xFF);
-		else if (texture_id == 1)
-			pixel_put(line_x, y, texture_x * 0xFF);
-		else if (texture_id == 2)
-			pixel_put(line_x, y, texture_x * 0xFF);
-		else if (texture_id == 3)
-			pixel_put(line_x, y, texture_x * 0xFF);
+		pixel_put(line_x, y, texture[texture_id][(int)(64 * (((float)y - line.x) / (line.y - line.x)))][(int)(64 * texture_x)]);
 		y++;
 	}
-	(void)texture_x;
+	while (y < WIN_HEIGHT)
+	{
+		pixel_put(line_x, y, 0x444444);
+		y++;
+	}
 }
