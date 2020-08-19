8fa9fd0aa845e4ef3be5b3396b1b66b6e281034b
rpehkone <pehkonenroni@gmail.com>
2020-08-19 21:36:55 +0300

    Started to implement sprites and added documentation and basic maps

--- src/wolf.c
+++ src/wolf.c
@@ -6,7 +6,7 @@
 /*   By: rpehkone <rpehkone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 22:01:47 by rpehkone          #+#    #+#             */
-/*   Updated: 2020/08/19 20:30:46 by rpehkone         ###   ########.fr       */
+/*   Updated: 2020/08/19 21:33:34 by rpehkone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,14 @@ static void	player_movement(t_float_xy *location,
 		rotate(direction, -0.05);
 	if (is_key_down(123))
 		rotate(direction, 0.05);
-	if (is_key_down(126) && 0 == map[(int)(location->x + direction->x * 10)][(int)location->y])
-		location->x += direction->x * 10;
-	if (is_key_down(126) && 0 == map[(int)location->x][(int)(location->y + direction->y * 10)])
-		location->y += direction->y * 10;
-	if (is_key_down(125) && 0 == map[(int)(location->x - direction->x * 10)][(int)location->y])
-		location->x -= direction->x * 10;
-	if (is_key_down(125) && 0 == map[(int)location->x][(int)(location->y - direction->y * 10)])
-		location->y -= direction->y * 10;
+	if (is_key_down(126) && 0 == map[(int)(location->x + direction->x * 100)][(int)location->y])
+		location->x += direction->x * 100;
+	if (is_key_down(126) && 0 == map[(int)location->x][(int)(location->y + direction->y * 100)])
+		location->y += direction->y * 100;
+	if (is_key_down(125) && 0 == map[(int)(location->x - direction->x * 100)][(int)location->y])
+		location->x -= direction->x * 100;
+	if (is_key_down(125) && 0 == map[(int)location->x][(int)(location->y - direction->y * 100)])
+		location->y -= direction->y * 100;
 	if (location->x < 1.1)
 		location->x = 1.1;
 	else if (location->x > map_size.y - 1.1)
@@ -78,6 +78,40 @@ static void	texture_offsets(t_float_xy direction,
 	}
 }
 
+static void	put_sprite(int x, int cast_length)
+{
+	//(void)cast_length;
+	for (int y = 0; y < cast_length / 10; y++)
+		pixel_put(x, WIN_HEIGHT / 2 + y, 0xFF0000);
+}
+
+static void	sprite(t_float_xy location, t_float_xy direction, int **map)
+{
+	t_float_xy	cast;
+	int			x;
+	int			cast_length;
+	static float fov = 0.00163625;
+	//static int	wmod = 310000;
+
+	rotate(&direction, -1 * fov * (WIN_WIDTH / 2));
+	x = 0;
+	while (x < WIN_WIDTH)
+	{
+		cast = location;
+		rotate(&direction, fov);
+		cast_length = 1;
+		while (map[(int)cast.x][(int)cast.y] == 0)
+		{
+			cast.x += direction.x;
+			cast.y += direction.y;
+			cast_length++;
+		}
+		if (map[(int)cast.x][(int)cast.y] == 3)
+			put_sprite(WIN_WIDTH - x - 1, cast_length);
+		x++;
+	}
+}
+
 static void	raycast(t_float_xy location, t_float_xy direction, int **map)
 {
 	t_float_xy	cast;
@@ -94,8 +128,8 @@ static void	raycast(t_float_xy location, t_float_xy direction, int **map)
 		fov += 0.0001;
 	if (is_key_down(21))
 		fov -= 0.0001;
-	printf("fov = %f\n", fov);
-	printf("wmod = %d\n", wmod);
+	//printf("fov = %f\n", fov);
+	//printf("wmod = %d\n", wmod);
 	rotate(&direction, -1 * fov * (WIN_WIDTH / 2));
 	x = 0;
 	while (x < WIN_WIDTH)
@@ -103,12 +137,13 @@ static void	raycast(t_float_xy location, t_float_xy direction, int **map)
 		cast = location;
 		rotate(&direction, fov);
 		cast_length = 1;
-		while (map[(int)cast.x][(int)cast.y] == 0)
+		while (map[(int)cast.x][(int)cast.y] != 1 && map[(int)cast.x][(int)cast.y] != 2)
 		{
 			cast.x += direction.x;
 			cast.y += direction.y;
 			cast_length++;
 		}
+		//if (map[(int)cast.x][(int)cast.y] != 3)
 		texture_offsets(direction, cast, wmod / cast_length,
 		WIN_WIDTH - x - 1, (map[(int)cast.x][(int)cast.y] - 1) * 4);
 		x++;
@@ -155,7 +190,7 @@ void	print_fractal(t_settings *settings)
 int			wolf(void)
 {
 	static t_float_xy	location = {.x = 1, .y = 1};
-	static t_float_xy	direction = {.x = 0.0, .y = .01};
+	static t_float_xy	direction = {.x = 0.0, .y = .001};
 	static t_int_xy		map_size;
 	static int			**map = NULL;
 
@@ -165,6 +200,7 @@ int			wolf(void)
 		map = read_map(NULL, &map_size);
 	player_movement(&location, &direction, map_size, map);
 	raycast(location, direction, map);
+	sprite(location, direction, map);
 	map_print(location, map_size, map);
 	update_image();
 	fps();
