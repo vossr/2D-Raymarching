74da345338be1e7e2d24a812d159a7790bda6211
rpehkone <pehkonenroni@gmail.com>
2020-08-24 16:30:12 +0300

    Fixed code format in render file

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: rpehkone <rpehkone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/16 19:52:10 by rpehkone          #+#    #+#             */
-/*   Updated: 2020/08/20 13:48:35 by rpehkone         ###   ########.fr       */
+/*   Updated: 2020/08/24 16:28:38 by rpehkone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,17 +31,17 @@ static unsigned char	**load_texture(int *line_s, int bps, int j)
 	files[8] = "textures/sandstone.xpm";
 	data = (unsigned char**)malloc(sizeof(unsigned char*) * 9);
 	mlx = get_mlx(NULL);
-	j = 0;
-	while (j < 9)
+	j = -1;
+	while (++j < 9)
 	{
 		texture[j] = mlx_xpm_file_to_image(mlx[0], files[j], &bps, line_s);
-		data[j] = (unsigned char*)mlx_get_data_addr(texture[j], &bps, line_s, &endian);
-		j++;
+		data[j] = (unsigned char*)mlx_get_data_addr(texture[j], &bps,
+											line_s, &endian);
 	}
 	return (data);
 }
 
-void			megapixel_put(int x, int y, int color)
+void					megapixel_put(int x, int y, int color)
 {
 	int	i;
 	int	j;
@@ -59,8 +59,9 @@ void			megapixel_put(int x, int y, int color)
 	}
 }
 
-void			map_print(t_settings *settings)
+void					map_print(t_settings *settings)
 {
+	t_float_xy	location2;
 	int			x;
 	int			y;
 
@@ -78,29 +79,21 @@ void			map_print(t_settings *settings)
 		}
 		y++;
 	}
-	t_float_xy location2;
 	location2.x = settings->location.x * 8;
 	location2.y = settings->location.y * 8;
 	megapixel_put((int)location2.x - 4, (int)location2.y - 4, 0xFF00);
 }
 
-void			put_texture(int line_x, t_float_xy line,
-					int texture_id, float texture_x)
+void					sky_and_cs_mode(int *texture_id, float *tex_size,
+								t_float_xy line, int line_x)
 {
-	static unsigned char	**texture = NULL;
-	int						line_s;
-	int						y;
-	float					tex_size;
+	int	y;
 
-	if (!texture)
-		texture = load_texture(&line_s, 0, 0);
-	tex_size = 64.0;
-	line_s = 256;
-	if (texture_id > 7)
+	*tex_size = 64.0;
+	if (*texture_id > 7)
 	{
-		texture_id = 8;
-		tex_size = 512.0;
-		line_s = 2024;
+		*texture_id = 8;
+		*tex_size = 512.0;
 	}
 	y = 0;
 	while (y < line.x)
@@ -108,19 +101,32 @@ void			put_texture(int line_x, t_float_xy line,
 		pixel_put(line_x, y, 0x87ceeb);
 		y++;
 	}
-	y = line.x;
-	if (y < 0)
-		y = 0;
-	int xd = (int)(tex_size * texture_x);
-	int yd;
+}
+
+void					put_texture(int line_x, t_float_xy line,
+							int texture_id, float texture_x)
+{
+	static unsigned char	**texture = NULL;
+	int						y;
+	float					tex_size;
+	int						xd;
+	int						yd;
+
+	if (!texture)
+		texture = load_texture(&y, 0, 0);
+	sky_and_cs_mode(&texture_id, &tex_size, line, line_x);
+	y = line.x < 0 ? 0 : line.x;
+	xd = (int)(tex_size * texture_x);
 	while (y < line.y && y < WIN_HEIGHT)
 	{
 		yd = (int)(tex_size * (((float)y - line.x) / (line.y - line.x)));
-		pixel_put(line_x, y, texture[texture_id][yd * line_s + xd * 4 + 2] * 0x10000 +
-				texture[texture_id][yd * line_s + xd * 4 + 1] * 0x100 + texture[texture_id][yd * line_s + xd * 4 + 0]);
+		pixel_put(line_x, y, texture[texture_id][yd * (int)(tex_size * 4) +
+			xd * 4 + 2] * 0x10000 + texture[texture_id][
+			yd * (int)(tex_size * 4) + xd * 4 + 1] * 0x100 +
+			texture[texture_id][yd * (int)(tex_size * 4) + xd * 4 + 0]);
 		y++;
 	}
 	y--;
 	while (++y < WIN_HEIGHT)
-		pixel_put(line_x, y, 0x444444);
+		pixel_put(line_x, y, texture_id == 8 ? 0xFFFFE0 : 0x444444);
 }
