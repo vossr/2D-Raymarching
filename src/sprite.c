6f87cc8136bddb0993c903ec7874cf0875b4bb29
rpehkone <pehkonenroni@gmail.com>
2020-09-04 20:07:28 +0300

    Got sprite x textureaccess almost working

--- src/sprite.c
+++ src/sprite.c
@@ -6,13 +6,13 @@
 /*   By: rpehkone <rpehkone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/24 19:47:35 by rpehkone          #+#    #+#             */
-/*   Updated: 2020/09/04 17:48:23 by rpehkone         ###   ########.fr       */
+/*   Updated: 2020/09/04 20:02:41 by rpehkone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
 
-void	put_sprite(int x, float x_offset, t_float_xy line)
+void	put_sprite(int x, int x_offset, t_float_xy line)
 {
 	void					*guard = NULL;
 	static unsigned char	*guard_data = NULL;
@@ -24,6 +24,7 @@ void	put_sprite(int x, float x_offset, t_float_xy line)
 	if (!guard_data)
 	{
 		mlx = get_mlx(NULL);
+		//check widht and height
 		guard = mlx_xpm_file_to_image(mlx[0],
 				"textures/guard.xpm", &bps, &line_s);
 		if (guard == NULL)
@@ -67,13 +68,40 @@ void	put_sprite(int x, float x_offset, t_float_xy line)
 	(void)x;
 }
 
+int		get_x_offset(t_float_xy sprite, t_float_xy cast, t_float_xy direction)
+{
+	float	center_dist;
+	float	prev_dist;
+	int		neg;
+
+	prev_dist = 100000;
+	neg = 0;
+	if (fabs(cast.x - sprite.x) - fabs(cast.y - sprite.y) < 0)
+		neg = 1;
+	center_dist = fabs(fabs(cast.x - sprite.x) - fabs(cast.y - sprite.y));
+	while (center_dist < prev_dist)
+	{
+		prev_dist = center_dist;
+		cast.x += direction.x;
+		cast.y += direction.y;
+		center_dist = fabs(fabs(cast.x - sprite.x) - fabs(cast.y - sprite.y));
+	}
+	//printf("res = %f\n", 360 * (0 + (center_dist / 0.4)));
+	if (!neg)
+	{
+		center_dist +=  0.2;
+		return ((int)(360 * (center_dist / 0.4)));
+	}
+	return (180 - (int)(360 * (center_dist / 0.4)));
+}
+
 void	sprite(t_settings *settings, int x, int stop, t_float_xy direction)
 {
 	t_float_xy		cast;
 	t_float_xy		line;
 	int				cast_length;
 
-	static t_float_xy	sprite = {.x = 3.0, .y = 3.0};
+	static t_float_xy	sprite = {.x = 3.5, .y = 2.0};
 	//sprite.x += .01;
 	//sprite.y += .01;
 	float sprite_dist = 1 * fabs(settings->location.x - sprite.x) + fabs((settings->location.y - sprite.y));
@@ -100,7 +128,7 @@ void	sprite(t_settings *settings, int x, int stop, t_float_xy direction)
 		}
 		//printf("cl = %d\n", cast_length);
 		//cast_length = sprite_dist;
-		printf("sd = %f\n", sprite_dist);
+	//	printf("sd = %f\n", sprite_dist);
 		(void)sprite_dist;
 	//	cast_length = 3000;
 		//if (settings->map[(int)cast.y][(int)cast.x] == '3')
@@ -112,9 +140,9 @@ void	sprite(t_settings *settings, int x, int stop, t_float_xy direction)
 				line.x = 0;
 			if (line.y > WIN_HEIGHT)
 				line.y = WIN_HEIGHT;
-			for (int y = line.x; y < line.y; y++)
-				pixel_put_blend(WIN_WIDTH - 1 - x, y, 0x11FF0000);
-			put_sprite(WIN_WIDTH - 1 - x, WIN_WIDTH - 1 - x, line);
+			//for (int y = line.x; y < line.y; y++)
+			//	pixel_put_blend(WIN_WIDTH - 1 - x, y, 0x11FF0000);
+			put_sprite(WIN_WIDTH - 1 - x, get_x_offset(sprite, cast, direction), line);
 		}
 	}
 	//put_sprite(WIN_WIDTH - 1 - x, line);
